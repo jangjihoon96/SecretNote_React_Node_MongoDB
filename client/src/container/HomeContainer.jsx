ae8ac6a221370238d7c350e611b8f1cc142d93fb
Jihoon <jangna96@gmail.com>
2023-09-19 18:25:00 +0900

    feat: Home Container 컴포넌트 제작

---
+++ client/src/container/HomeContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function HomeContainer({ children }) {
+  return <StyledHomeContainer>{children}</StyledHomeContainer>;
+}
+
+const StyledHomeContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  padding: 3.125rem;
+  box-sizing: border-box;
+`;
