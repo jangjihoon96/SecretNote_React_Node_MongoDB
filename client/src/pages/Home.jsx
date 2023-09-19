778e653f6a3680dbaacdfff919d3561fd98f6ddf
Jihoon <jangna96@gmail.com>
2023-09-19 18:42:52 +0900

    feat: Home 컴포넌트 마크업 제작

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import HomeContainer from "../container/HomeContainer";
+import MainLogo from "../components/home/MainLogo";
+import SubTitle from "../components/home/SubTitle";
+import {
+  Button,
+  Form,
+  Input,
+  Inputs,
+  LinkButton,
+} from "../components/common/Common";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link to="/board">게시판</Link>
-      <Link to="/signup">회원가입</Link>
-      <Link to="/write">글쓰기</Link>
-    </div>
+    <HomeContainer>
+      <MainLogo />
+      <SubTitle>나만의 작은 비밀 노트</SubTitle>
+      <Form>
+        <Inputs>
+          <Input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="E-mail을 입력하세요."
+          />
+          <Input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="비밀번호를 입력하세요."
+          />
+        </Inputs>
+        <Button color="blue">로그인</Button>
+        <LinkButton to="/signup">회원가입</LinkButton>
+      </Form>
+    </HomeContainer>
   );
 }
