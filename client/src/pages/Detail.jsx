5f1a0491cb22612eeafe424be6d979eef6710c9e
Jihoon <jangna96@gmail.com>
2023-09-22 17:29:19 +0900

    feat: 게시물 디테일 컴포넌트 렌더링 기능 구현
    - useParams()를 통해 id 값 추출
    - 값을 담을 state 생성
    - 서버 api 통신, 추출한 id 값 query를 통해 값을 보냄
    - 응답한 값 state 저장 후 렌더링

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Detail() {
-  return <div>Detail</div>;
+  const { id } = useParams();
+  const [postDetail, setPostDetail] = useState({});
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/posts/read/detail?id=${id}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log(res);
+        setPostDetail(res);
+      });
+  }, [id]);
+  return (
+    <div>
+      <h2>{postDetail.title}</h2>
+      <p>{postDetail.content}</p>
+      <p>{postDetail.date}</p>
+    </div>
+  );
 }
